5581f1cac9f5bb5e6560e0360033ab4b89b80740
กังฟู 602 <klaw.amz@hotmail.com>
2015-10-20 15:59:25 +0700

    Quiz

---
+++ MrRobot/HackedModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrRobot
+{
+    class HackedModel : MyModel 
+    {
+        public override void PerformSendMessage()
+        {
+            Console.WriteLine("message: 570610590");
+            
+        }
+    }
+}
